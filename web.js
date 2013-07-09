6111f7a82c0e5cf90b660d9f99cf79da982011b4
Dusko Ojdanic <dusko.ojdanic@gmail.com>
2013-07-09 22:53:19 +0000

    web.js reads from file into a buffer

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+
+var fs = require('fs');
+
+var buff = new Buffer(100);
+
+buff = fs.readFileSync('index.html', 'utf-8');
+
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buff.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
